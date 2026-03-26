4840008df9a600f718acfb30d6e956d3d1169fcb
Swarnava Neogi <swarnavaneogi@gmail.com>
2026-03-26 12:47:28 +0530

    Add C program to demonstrate pointer basics including value, address, and dereferencing

---
+++ Basic/basic_pointer.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main()
+{
+    int n;
+    printf("Enter a number: ");
+    scanf("%d", &n);
+
+    int *p = &n;
+
+    printf("Value using variable: %d\n", n);
+    printf("Address using & operator: %p\n", (void*)&n);
+    printf("Value using pointer: %d\n", *p);
+    printf("Address stored in pointer: %p\n", (void*)p);
+
+    return 0;
+}
